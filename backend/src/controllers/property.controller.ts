6fb99ee565e9dee56d7796bf768faf30b73435de
SushilPrajapati1 <soucesil1@gmail.com>
2026-03-29 22:25:51 +0545

    feat: added isFavorite flag in properties response

--- backend/src/controllers/property.controller.ts
+++ backend/src/controllers/property.controller.ts
@@ -1,16 +1,37 @@
 import type { Request, Response } from "express";
 import { catchAsync } from "../utils/catchAsync.js";
 import * as propertyService from "../services/property.service.js";
+import * as favoriteService from "../services/favorite.service.js";
 
 export const getProperties = catchAsync(async (req: Request, res: Response) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
+  const userId = req.user?.id;
+
   const { property, total } = await propertyService.getAllProperties(
     page,
     limit,
   );
   const totalPages = Math.ceil(total / limit);
   const nextPage = page < totalPages ? page + 1 : null;
+  if (userId && typeof userId === "number") {
+    const favorites = await favoriteService.getFavorites(userId);
+    const favoritesIds = favorites.map((item) => item.propertyId);
+    const propertieswithFavFlag = property.map((item) => ({
+      ...item,
+      isFavorite: favoritesIds.includes(item.id),
+    }));
+    return res.status(200).json({
+      status: "success",
+      total,
+      page,
+      limit,
+      totalPages,
+      nextPage,
+      data: propertieswithFavFlag,
+    });
+  }
+
   res.status(200).json({
     status: "success",
     total,
